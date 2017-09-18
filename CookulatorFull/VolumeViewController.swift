730facea406eb4f1a75612288d1de2963b601332
Petra Dueck <petra@node-1w7jr9qkaqaipvvz3dv55nzzo.ipv6.telus.net>
2017-09-17 22:20:25 -0700

    volume refinements

--- CookulatorFull/VolumeViewController.swift
+++ CookulatorFull/VolumeViewController.swift
@@ -23,6 +23,51 @@ class VolumeViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var free_ausTbspTextField: UITextField!
     @IBOutlet weak var free_ukCanAusTspTextField: UITextField!
     
+    //@IBOutlet weak var mlTextField: UITextField!
+    //@IBOutlet weak var usCupTextField: UITextField!
+    //@IBOutlet weak var usFlOzTextField: UITextField!
+    //@IBOutlet weak var usTbspTextField: UITextField!
+    //@IBOutlet weak var usTspTextField: UITextField!
+    //@IBOutlet weak var ukCupTextField: UITextField!
+    //@IBOutlet weak var canAusCupTextField: UITextField!
+    //@IBOutlet weak var ukCanAusFlOzTextField: UITextField!
+    //@IBOutlet weak var ukCanTbspTextField: UITextField!
+    //@IBOutlet weak var ausTbspTextField: UITextField!
+    //@IBOutlet weak var ukCanAusTspTextField: UITextField!
+    
+    @IBOutlet weak var literTextField: UITextField!
+    @IBOutlet weak var metricDropTextField: UITextField!
+    
+    @IBOutlet weak var ukDspTextField: UITextField!
+    @IBOutlet weak var usDramTextField: UITextField!
+    @IBOutlet weak var ukDramTextField: UITextField!
+    @IBOutlet weak var usDashTextField: UITextField!
+    @IBOutlet weak var ukDashTextField: UITextField!
+    @IBOutlet weak var usPinchTextField: UITextField!
+    @IBOutlet weak var ukPinchTextField: UITextField!
+    @IBOutlet weak var usSmidgenTextField: UITextField!
+    @IBOutlet weak var ukSmidgenTextField: UITextField!
+    @IBOutlet weak var usDropTextField: UITextField!
+    @IBOutlet weak var ukDropTextField: UITextField!
+    
+    @IBOutlet weak var usFlGallonTextField: UITextField!
+    @IBOutlet weak var usDrGallonTextField: UITextField!
+    @IBOutlet weak var ukGallonTextField: UITextField!
+    @IBOutlet weak var usFlQuartTextField: UITextField!
+    @IBOutlet weak var usDrQuartTextField: UITextField!
+    @IBOutlet weak var ukQuartTextField: UITextField!
+    @IBOutlet weak var usFlPintTextField: UITextField!
+    @IBOutlet weak var usDrPintTextField: UITextField!
+    @IBOutlet weak var ukPintTextField: UITextField!
+    @IBOutlet weak var usGillTextField: UITextField!
+    @IBOutlet weak var ukGillTextField: UITextField!
+    
+    @IBOutlet weak var usLegalCupTextField: UITextField!
+    @IBOutlet weak var usLegalFlOzTextField: UITextField!
+    @IBOutlet weak var usLegalTspTextField: UITextField!
+    
+    @IBOutlet weak var japCupTextField: UITextField!
+    
     var mlFloat : Float?
     var convertedFromString : String?
     
@@ -118,6 +163,130 @@ class VolumeViewController: UIViewController, UITextFieldDelegate {
                 returnMlFloat = VolumeConverter.ukTspToMl(senderFloatValue)
                 returnConvertedFromString = "UK/Can/Aus tsp"
                 
+                
+            // pay version
+            /*
+            case usCupTextField:
+                returnMlFloat = VolumeConverter.usCupToMl(senderFloatValue)
+                returnConvertedFromString = "US cups"
+            case usFlOzTextField:
+                returnMlFloat = VolumeConverter.usFlOzToMl(senderFloatValue)
+                returnConvertedFromString = "US fluid oz"
+            case usTbspTextField:
+                returnMlFloat = VolumeConverter.usTbspToMl(senderFloatValue)
+                returnConvertedFromString = "US tbsp"
+            case usTspTextField:
+                returnMlFloat = VolumeConverter.usTspToMl(senderFloatValue)
+                returnConvertedFromString = "US tsp"
+            case ukCupTextField:
+                returnMlFloat = VolumeConverter.ukCupToMl(senderFloatValue)
+                returnConvertedFromString = "UK cups"
+            case canAusCupTextField:
+                returnMlFloat = VolumeConverter.metricCupsToMl(senderFloatValue)
+                returnConvertedFromString = "Can/Aus cups"
+            case ukCanAusFlOzTextField:
+                returnMlFloat = VolumeConverter.ukFlOzToMl(senderFloatValue)
+                returnConvertedFromString = "UK/Can/Aus fluid oz"
+            case ukCanTbspTextField:
+                returnMlFloat = VolumeConverter.ukTbspToMl(senderFloatValue)
+                returnConvertedFromString = "UK/Can tbsp"
+            case ausTbspTextField:
+                returnMlFloat = VolumeConverter.ausTbspToMl(senderFloatValue)
+                returnConvertedFromString = "Aus tbsp"
+            case ukCanAusTspTextField:
+                returnMlFloat = VolumeConverter.ukTspToMl(senderFloatValue)
+                returnConvertedFromString = "UK/Can/Aus tsp"
+            */
+                
+            case literTextField:
+                returnMlFloat = VolumeConverter.metricLiterToMl(senderFloatValue)
+                returnConvertedFromString = "metric liter"
+            case metricDropTextField:
+                returnMlFloat = VolumeConverter.metricDropToMl(senderFloatValue)
+                returnConvertedFromString = "metric drop"
+            case ukDspTextField:
+                returnMlFloat = VolumeConverter.ukDspToMl(senderFloatValue)
+                returnConvertedFromString = "UK dsp"
+            case usDramTextField:
+                returnMlFloat = VolumeConverter.usDramToMl(senderFloatValue)
+                returnConvertedFromString = "US dram"
+            case ukDramTextField:
+                returnMlFloat = VolumeConverter.ukDramToMl(senderFloatValue)
+                returnConvertedFromString = "UK dram"
+            case usDashTextField:
+                returnMlFloat = VolumeConverter.usDashToMl(senderFloatValue)
+                returnConvertedFromString = "US dash"
+            case ukDashTextField:
+                returnMlFloat = VolumeConverter.ukDashToMl(senderFloatValue)
+                returnConvertedFromString = "UK dash"
+            case usPinchTextField:
+                returnMlFloat = VolumeConverter.usPinchToMl(senderFloatValue)
+                returnConvertedFromString = "US pinch"
+            case ukPinchTextField:
+                returnMlFloat = VolumeConverter.ukPinchToMl(senderFloatValue)
+                returnConvertedFromString = "UK pinch"
+            case usSmidgenTextField:
+                returnMlFloat = VolumeConverter.usSmidgenToMl(senderFloatValue)
+                returnConvertedFromString = "US smidgen"
+            case ukSmidgenTextField:
+                returnMlFloat = VolumeConverter.ukSmidgenToMl(senderFloatValue)
+                returnConvertedFromString = "UK smidgen"
+            case usDropTextField:
+                returnMlFloat = VolumeConverter.usDropToMl(senderFloatValue)
+                returnConvertedFromString = "US drop"
+            case ukDropTextField:
+                returnMlFloat = VolumeConverter.ukDropToMl(senderFloatValue)
+                returnConvertedFromString = "UK drop"
+                
+            case usFlGallonTextField:
+                returnMlFloat = VolumeConverter.usFlGallonToMl(senderFloatValue)
+                returnConvertedFromString = "US fluid gallon"
+            case usDrGallonTextField:
+                returnMlFloat = VolumeConverter.usDryGallonToMl(senderFloatValue)
+                returnConvertedFromString = "US dry gallon"
+            case ukGallonTextField:
+                returnMlFloat = VolumeConverter.ukGallonToMl(senderFloatValue)
+                returnConvertedFromString = "UK gallon"
+            case usFlQuartTextField:
+                returnMlFloat = VolumeConverter.usFlQuartToMl(senderFloatValue)
+                returnConvertedFromString = "US fluid quart"
+            case usDrQuartTextField:
+                returnMlFloat = VolumeConverter.usDryQuartToMl(senderFloatValue)
+                returnConvertedFromString = "US dry quart"
+            case ukQuartTextField:
+                returnMlFloat = VolumeConverter.ukQuartToMl(senderFloatValue)
+                returnConvertedFromString = "UK quart"
+            case usFlPintTextField:
+                returnMlFloat = VolumeConverter.usFlPintToMl(senderFloatValue)
+                returnConvertedFromString = "US fluid pint"
+            case usDrPintTextField:
+                returnMlFloat = VolumeConverter.usDryPintToMl(senderFloatValue)
+                returnConvertedFromString = "US dry pint"
+            case ukPintTextField:
+                returnMlFloat = VolumeConverter.ukPintToMl(senderFloatValue)
+                returnConvertedFromString = "UK pint"
+            case usGillTextField:
+                returnMlFloat = VolumeConverter.usGillToMl(senderFloatValue)
+                returnConvertedFromString = "US gill"
+            case ukGillTextField:
+                returnMlFloat = VolumeConverter.ukGillToMl(senderFloatValue)
+                returnConvertedFromString = "UK gill"
+                
+            case usLegalCupTextField:
+                returnMlFloat = VolumeConverter.usLegalCupsToMl(senderFloatValue)
+                returnConvertedFromString = "US legal cups"
+            case usLegalFlOzTextField:
+                returnMlFloat = VolumeConverter.usLegalFlOzToMl(senderFloatValue)
+                returnConvertedFromString = "US legal fluid oz"
+            case usLegalTspTextField:
+                returnMlFloat = VolumeConverter.usLegalTspToMl(senderFloatValue)
+                returnConvertedFromString = "US legal tsp"
+                
+            case japCupTextField:
+                returnMlFloat = VolumeConverter.japCupsToMl(senderFloatValue)
+                returnConvertedFromString = "Japanese cups"
+                
+                
             default:
                 print("don't know what text field this is")
                 break
@@ -156,6 +325,56 @@ class VolumeViewController: UIViewController, UITextFieldDelegate {
         self.free_ukCanAusTspTextField.text = NSString(format: "%.2f", VolumeConverter.mlToUkTsp(mlToConvert)) as String
         
         self.free_messageLabel.text = "Converted from \(convertedFrom)"
+        
+        
+        // pay version
+        /*
+        self.mlTextField.text = NSString(format: "%.0f", mlToConvert) as String
+        self.usCupTextField.text = NSString(format: "%.2f", VolumeConverter.mlToUsCups(mlToConvert)) as String
+        self.usFlOzTextField.text = NSString(format: "%.2f", VolumeConverter.mlToUsFlOz(mlToConvert)) as String
+        self.usTbspTextField.text = NSString(format: "%.2f", VolumeConverter.mlToUsTbsp(mlToConvert)) as String
+        self.usTspTextField.text = NSString(format: "%.2f", VolumeConverter.mlToUsTsp(mlToConvert)) as String
+        self.ukCupTextField.text = NSString(format: "%.2f", VolumeConverter.mlToUkCups(mlToConvert)) as String
+        self.canAusCupTextField.text = NSString(format: "%.2f", VolumeConverter.mlToMetricCups(mlToConvert)) as String
+        self.ukCanAusFlOzTextField.text = NSString(format: "%.2f", VolumeConverter.mlToUkFlOz(mlToConvert)) as String
+        self.ukCanTbspTextField.text = NSString(format: "%.2f", VolumeConverter.mlToUkTbsp(mlToConvert)) as String
+        self.ausTbspTextField.text = NSString(format: "%.2f", VolumeConverter.mlToAusTbsp(mlToConvert)) as String
+        self.ukCanAusTspTextField.text = NSString(format: "%.2f", VolumeConverter.mlToUkTsp(mlToConvert)) as String
+        
+        self.messageLabel.text = "Converted from \(convertedFrom)"
+        */
+        
+        self.literTextField.text = NSString(format: "%.2f", VolumeConverter.mlToMetricLiter(mlToConvert)) as String
+        self.metricDropTextField.text = NSString(format: "%.2f", VolumeConverter.mlToMetricDrop(mlToConvert)) as String
+        self.ukDspTextField.text = NSString(format: "%.2f", VolumeConverter.mlToUkDsp(mlToConvert)) as String
+        self.usDramTextField.text = NSString(format: "%.2f", VolumeConverter.mlToUsDram(mlToConvert)) as String
+        self.ukDramTextField.text = NSString(format: "%.2f", VolumeConverter.mlToUkDram(mlToConvert)) as String
+        self.usDashTextField.text = NSString(format: "%.2f", VolumeConverter.mlToUsDash(mlToConvert)) as String
+        self.ukDashTextField.text = NSString(format: "%.2f", VolumeConverter.mlToUkDash(mlToConvert)) as String
+        self.usPinchTextField.text = NSString(format: "%.2f", VolumeConverter.mlToUsPinch(mlToConvert)) as String
+        self.ukPinchTextField.text = NSString(format: "%.2f", VolumeConverter.mlToUkPinch(mlToConvert)) as String
+        self.usSmidgenTextField.text = NSString(format: "%.2f", VolumeConverter.mlToUsSmidgen(mlToConvert)) as String
+        self.ukSmidgenTextField.text = NSString(format: "%.2f", VolumeConverter.mlToUkSmidgen(mlToConvert)) as String
+        self.usDropTextField.text = NSString(format: "%.2f", VolumeConverter.mlToUsDrop(mlToConvert)) as String
+        self.ukDropTextField.text = NSString(format: "%.2f", VolumeConverter.mlToUkDrop(mlToConvert)) as String
+        
+        self.usFlGallonTextField.text = NSString(format: "%.2f", VolumeConverter.mlToUsFlGallon(mlToConvert)) as String
+        self.usDrGallonTextField.text = NSString(format: "%.2f", VolumeConverter.mlToUsDryGallon(mlToConvert)) as String
+        self.ukGallonTextField.text = NSString(format: "%.2f", VolumeConverter.mlToUkGallon(mlToConvert)) as String
+        self.usFlQuartTextField.text = NSString(format: "%.2f", VolumeConverter.mlToUsFlQuart(mlToConvert)) as String
+        self.usDrQuartTextField.text = NSString(format: "%.2f", VolumeConverter.mlToUsDryQuart(mlToConvert)) as String
+        self.ukQuartTextField.text = NSString(format: "%.2f", VolumeConverter.mlToUkQuart(mlToConvert)) as String
+        self.usFlPintTextField.text = NSString(format: "%.2f", VolumeConverter.mlToUsFlPint(mlToConvert)) as String
+        self.usDrPintTextField.text = NSString(format: "%.2f", VolumeConverter.mlToUsDryPint(mlToConvert)) as String
+        self.ukPintTextField.text = NSString(format: "%.2f", VolumeConverter.mlToUkPint(mlToConvert)) as String
+        self.usGillTextField.text = NSString(format: "%.2f", VolumeConverter.mlToUsGill(mlToConvert)) as String
+        self.ukGillTextField.text = NSString(format: "%.2f", VolumeConverter.mlToUkGill(mlToConvert)) as String
+        
+        self.usLegalCupTextField.text = NSString(format: "%.2f", VolumeConverter.mlToUsLegalCups(mlToConvert)) as String
+        self.usLegalFlOzTextField.text = NSString(format: "%.2f", VolumeConverter.mlToUsLegalFlOz(mlToConvert)) as String
+        self.usLegalTspTextField.text = NSString(format: "%.2f", VolumeConverter.mlToUsLegalTsp(mlToConvert)) as String
+        
+        self.japCupTextField.text = NSString(format: "%.2f", VolumeConverter.mlToJapCups(mlToConvert)) as String
         
         return true
         
