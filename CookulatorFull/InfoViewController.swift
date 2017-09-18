e4f09f16b21ae488a45abe00d87accbce56f1e02
Petra Dueck <petra@node-1w7jr9qkaqaipkekvomupo0bc.ipv6.telus.net>
2017-09-17 20:56:56 -0700

    volume page layout done

---
+++ CookulatorFull/InfoViewController.swift
@@ -0,0 +1,24 @@
+//
+//  InfoViewController.swift
+//  CookulatorFull
+//
+//  Created by Petra Dueck on 2017-09-17.
+//  Copyright © 2017 Petra Dueck. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
